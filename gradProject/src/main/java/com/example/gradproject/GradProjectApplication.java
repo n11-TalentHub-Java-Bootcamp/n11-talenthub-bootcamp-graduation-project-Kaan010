f6448be188aa35157fc60e9233fc3442b2ccd78b
kaankalan <kaankalan@outlook.com.tr>
2022-01-28 21:23:17 +0300

    initial commit

---
+++ gradProject/src/main/java/com/example/gradproject/GradProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.gradproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradProjectApplication.class, args);
+    }
+
+}
