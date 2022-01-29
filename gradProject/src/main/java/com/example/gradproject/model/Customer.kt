a28e14c46c547516872a4ac29356680ea06b1552
kaankalan <kaankalan@outlook.com.tr>
2022-01-29 21:13:31 +0300

    customer entity equals and hashcode implemented

--- gradProject/src/main/java/com/example/gradproject/model/Customer.kt
+++ gradProject/src/main/java/com/example/gradproject/model/Customer.kt
@@ -24,5 +24,36 @@ class Customer @JvmOverloads constructor(
         val	assurance: Int?,
         var creditNote:Int?
 ){
+        override fun equals(other: Any?): Boolean {
+                if (this === other) return true
+                if (javaClass != other?.javaClass) return false
+
+                other as Customer
+
+                if (id != other.id) return false
+                if (identityNumber != other.identityNumber) return false
+                if (name != other.name) return false
+                if (surName != other.surName) return false
+                if (salary != other.salary) return false
+                if (telephone != other.telephone) return false
+                if (birthDate != other.birthDate) return false
+                if (assurance != other.assurance) return false
+                if (creditNote != other.creditNote) return false
+
+                return true
+        }
+
+        override fun hashCode(): Int {
+                var result = id?.hashCode() ?: 0
+                result = 31 * result + identityNumber.hashCode()
+                result = 31 * result + name.hashCode()
+                result = 31 * result + surName.hashCode()
+                result = 31 * result + salary
+                result = 31 * result + telephone.hashCode()
+                result = 31 * result + birthDate.hashCode()
+                result = 31 * result + (assurance ?: 0)
+                result = 31 * result + (creditNote ?: 0)
+                return result
+        }
 
 }
