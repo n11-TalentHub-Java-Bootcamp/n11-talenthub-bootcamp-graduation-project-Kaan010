c2d1ebb0cf2854e155e2f4903defa5d9af61ff6c
kaankalan <kaankalan@outlook.com.tr>
2022-01-30 02:38:17 +0300

    sms feature is added

--- gradProject/src/main/java/com/example/gradproject/service/CreditApplicationService.java
+++ gradProject/src/main/java/com/example/gradproject/service/CreditApplicationService.java
@@ -1,11 +1,14 @@
 package com.example.gradproject.service;
 
+import com.example.gradproject.smsConfig.SmsRequest;
+import com.example.gradproject.smsConfig.SmsService;
 import com.example.gradproject.dto.CreditApplicationDto;
 import com.example.gradproject.dto.CreditApplicationRequest;
 import com.example.gradproject.dto.converter.CreditApplicationDtoConverter;
 import com.example.gradproject.exception.CreditApplicationCRUDException;
 import com.example.gradproject.model.CreditApplication;
 import com.example.gradproject.model.CreditApplicationId;
+import com.example.gradproject.model.CreditResult;
 import com.example.gradproject.model.Customer;
 import com.example.gradproject.repository.CreditApplicationRepository;
 import com.example.gradproject.util.CreditResultCalculatorUtil;
@@ -19,16 +22,18 @@ import java.util.stream.Collectors;
 @Service
 public class CreditApplicationService {
 
+    private final SmsService smsService;
     private final CustomerService customerService;
     private final CreditApplicationRepository creditApplicationRepository;
 
-    public CreditApplicationService(CustomerService customerService, CreditApplicationRepository creditApplicationRepository) {
+    public CreditApplicationService(SmsService smsService, CustomerService customerService, CreditApplicationRepository creditApplicationRepository) {
+        this.smsService = smsService;
         this.customerService = customerService;
         this.creditApplicationRepository = creditApplicationRepository;
     }
 
-    private static void sendSMS() {
-        //sasasa;
+    private void sendSMS(SmsRequest smsRequest) {
+        smsService.sendSms(smsRequest);
     }
 
     public CreditApplicationDto createCreditApplicationForCustomer(String customerId) {
@@ -37,6 +42,16 @@ public class CreditApplicationService {
             Customer customer = applicant.get();
             CreditApplication creditApplication = getNewCreditApplication(customer);
             creditApplicationRepository.save(creditApplication);
+            sendSMS(
+                    new SmsRequest(
+                            creditApplication.getCustomerTelephoneNo(),
+                            "\nDear "+customer.getName()+" "+customer.getSurName()+",\n"+
+                            "Your credit application has been announced. " +
+                                    (creditApplication.getCreditResult().equals(CreditResult.DENIED) ?
+                                            ("We are sorry to say that your application has been rejected.") :
+                                            ("We are happy to say that your application has been approved. Your limit is " + creditApplication.getCreditLimit())
+                                                    + "Have a good days. sent from n11.com by Kaan Kalan.")
+                    ));
             return CreditApplicationDtoConverter.convertCreditApplicationToCreditApplicationDto(creditApplication);
         } else throw new CreditApplicationCRUDException("Credit Application could not created");
     }
@@ -69,6 +84,6 @@ public class CreditApplicationService {
                         CreditApplicationDtoConverter.convertCreditApplicationRequestToCreditApplicationId(creditApplicationRequest)
                 )
                 .map(CreditApplicationDtoConverter::convertCreditApplicationToCreditApplicationDto)
-                .orElseThrow(()-> new CreditApplicationCRUDException("There is no credit application with this id"));
+                .orElseThrow(() -> new CreditApplicationCRUDException("There is no credit application with this id"));
     }
 }
